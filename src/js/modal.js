589b6165d90e0ae7cbe7becec95705f10f76a4c1
lmcgrana <lmcgrana@indiana.edu>
2017-07-05 13:23:25 -0400

    Stubs out modal markup and styles

--- src/js/modal.js
+++ src/js/modal.js
@@ -16,96 +16,97 @@
 
 
 function Modal(modalName) {
-	this.modalEl = document.querySelector('#'+modalName);
-    if(!this.modalEl) {
-        console.error("Could not find modal element #"+modalName);
+    this.modalEl = document.querySelector('#' + modalName);
+    if (!this.modalEl) {
+        console.error("Could not find modal element #" + modalName);
         return;
     }
-	this.overlayEl = document.querySelector('.uitsds-modal-overlay');
-	this.focusedElBeforeOpen;
+    this.overlayEl = document.querySelector('.uitsds-modal-overlay');
+    this.focusedElBeforeOpen;
 
-	var focusableEls = this.modalEl.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]');
-	this.focusableEls = Array.prototype.slice.call(focusableEls);
+    var focusableEls = this.modalEl.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]');
+    this.focusableEls = Array.prototype.slice.call(focusableEls);
 
-	this.firstFocusableEl = this.focusableEls[0];
-	this.lastFocusableEl = this.focusableEls[ this.focusableEls.length - 1 ];
+    this.firstFocusableEl = this.focusableEls[0];
+    this.lastFocusableEl = this.focusableEls[this.focusableEls.length - 1];
 
     this.addEventListeners(
-        ".open-modal[data-modal='"+modalName+"']",
-        '#'+modalName+' .close-modal');
+        ".open-modal[data-modal='" + modalName + "']",
+        '#' + modalName + ' .close-modal');
 
-	this.close(); // Reset
+    this.close(); // Reset
 }
 
 
 Modal.prototype.open = function() {
 
-	var Modal = this;
+    var Modal = this;
 
-	this.modalEl.removeAttribute('aria-hidden');
-	this.overlayEl.removeAttribute('aria-hidden');
+    this.modalEl.removeAttribute('aria-hidden');
+    this.overlayEl.removeAttribute('aria-hidden');
 
-	this.focusedElBeforeOpen = document.activeElement;
+    this.focusedElBeforeOpen = document.activeElement;
 
-	this.modalEl.addEventListener('keydown', function(e) {
-		Modal._handleKeyDown(e);
-	});
+    this.modalEl.addEventListener('keydown', function(e) {
+        Modal._handleKeyDown(e);
+    });
 
-	this.overlayEl.addEventListener('click', function() {
-		Modal.close();
-	});
+    this.overlayEl.addEventListener('click', function() {
+        Modal.close();
+    });
 
-	this.firstFocusableEl.focus();
+    this.firstFocusableEl.focus();
 };
 
 Modal.prototype.close = function() {
 
-	this.modalEl.setAttribute('aria-hidden', true);
-	this.overlayEl.setAttribute('aria-hidden', true);
+    this.modalEl.setAttribute('aria-hidden', true);
+    this.overlayEl.setAttribute('aria-hidden', true);
 
-	if ( this.focusedElBeforeOpen ) {
-		this.focusedElBeforeOpen.focus();
-	}
+    if (this.focusedElBeforeOpen) {
+        this.focusedElBeforeOpen.focus();
+    }
 };
 
 
 Modal.prototype._handleKeyDown = function(e) {
 
-	var Modal = this;
-	var KEY_TAB = 9;
-	var KEY_ESC = 27;
+    var Modal = this;
+    var KEY_TAB = 9;
+    var KEY_ESC = 27;
 
-	function handleBackwardTab() {
-		if ( document.activeElement === Modal.firstFocusableEl ) {
-			e.preventDefault();
-			Modal.lastFocusableEl.focus();
-		}
-	}
-	function handleForwardTab() {
-		if ( document.activeElement === Modal.lastFocusableEl ) {
-			e.preventDefault();
-			Modal.firstFocusableEl.focus();
-		}
-	}
+    function handleBackwardTab() {
+        if (document.activeElement === Modal.firstFocusableEl) {
+            e.preventDefault();
+            Modal.lastFocusableEl.focus();
+        }
+    }
 
-	switch(e.keyCode) {
-	case KEY_TAB:
-		if ( Modal.focusableEls.length === 1 ) {
-			e.preventDefault();
-			break;
-		}
-		if ( e.shiftKey ) {
-			handleBackwardTab();
-		} else {
-			handleForwardTab();
-		}
-		break;
-	case KEY_ESC:
-		Modal.close();
-		break;
-	default:
-		break;
-	}
+    function handleForwardTab() {
+        if (document.activeElement === Modal.lastFocusableEl) {
+            e.preventDefault();
+            Modal.firstFocusableEl.focus();
+        }
+    }
+
+    switch (e.keyCode) {
+        case KEY_TAB:
+            if (Modal.focusableEls.length === 1) {
+                e.preventDefault();
+                break;
+            }
+            if (e.shiftKey) {
+                handleBackwardTab();
+            } else {
+                handleForwardTab();
+            }
+            break;
+        case KEY_ESC:
+            Modal.close();
+            break;
+        default:
+            break;
+    }
 
 
 };
@@ -113,20 +114,20 @@ Modal.prototype._handleKeyDown = function(e) {
 
 Modal.prototype.addEventListeners = function(openModalSel, closeModalSel) {
 
-	var Modal = this;
+    var Modal = this;
 
-	var openModalEls = document.querySelectorAll(openModalSel);
-	for ( var i = 0; i < openModalEls.length; i++ ) {
-		openModalEls[i].addEventListener('click', function() {
-			Modal.open();
-		});
-	}
+    var openModalEls = document.querySelectorAll(openModalSel);
+    for (var i = 0; i < openModalEls.length; i++) {
+        openModalEls[i].addEventListener('click', function() {
+            Modal.open();
+        });
+    }
 
-	var closeModalEls = document.querySelectorAll(closeModalSel);
-	for ( var i = 0; i < closeModalEls.length; i++ ) {
-		closeModalEls[i].addEventListener('click', function() {
-			Modal.close();
-		});
-	}
+    var closeModalEls = document.querySelectorAll(closeModalSel);
+    for (var i = 0; i < closeModalEls.length; i++) {
+        closeModalEls[i].addEventListener('click', function() {
+            Modal.close();
+        });
+    }
 
 };
