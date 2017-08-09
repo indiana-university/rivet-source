809e306149cef64f12db21ff654a85a0c162e98d
lmcgrana <lmcgrana@iu.edu>
2017-08-09 16:47:43 -0400

    Adds header taskt to add version numbers to compiled files.

--- config/css.js
+++ config/css.js
@@ -2,10 +2,21 @@ const gulp = require('gulp');
 const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
 const runSequence = require('run-sequence');
+const header = require('gulp-header');
+const package = require('../package.json');
 
 gulp.task('css:dist', function() {
     return gulp.src('static/css/rivet.css')
         .pipe(gulp.dest('dist/css'));
+});
+
+// Create the string for the verion number banner.
+var banner = '/*! <%= package.name %> - @version v<%= package.version %> */' + '\n' + '\n';
+
+gulp.task('css:header', function() {
+    return gulp.src('dist/css/rivet.css')
+        .pipe(header(banner, { package : package }))
+        .pipe(gulp.dest('dist/css/'))
 });
 
 
@@ -19,5 +30,5 @@ gulp.task('css:minify', function() {
 });
 
 gulp.task('css:release', function(done) {
-    runSequence('css:dist', 'css:minify', done);
+    runSequence('css:dist', 'css:header', 'css:minify', done);
 });
