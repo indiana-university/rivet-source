0c942cc630967db3416155596ba70c8e4052abe9
Austin Lord <ahlord@iu.edu>
2017-08-17 14:46:48 -0400

    change prodUrl for analytics to enable testing

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -3,7 +3,7 @@ window.intercomSettings = {app_id: 'jv1pihi0'}; // UXO Intercom account
 (function(){var w=window;var ic=w.Intercom;if(typeof ic==='function'){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/jv1pihi0';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
 
 /* add google analytics if this is on production */
-var prodUrl = 'https://uitsdsgn.webtest.iu.edu/uitsds/';
+var prodUrl = 'https://uitsdsgn.webtest.iu.edu/feature-intercom/';
 if(document.location.href.search(prodUrl)!==-1) {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
