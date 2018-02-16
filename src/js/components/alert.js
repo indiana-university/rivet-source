ba1426ff7dda50b80cdbc434dd97db4fc08e287c
lmcgrana <lmcgrana@iu.edu>
2018-02-16 14:22:57 -0500

    Refactors Alert component to delegate event listening to the document
    This makes it so that dynamically added alerts will still be functional. Since the event lisenter is on the document and we are just catching the event target we don't need to listen to individual alert dismiss buttons.

--- src/js/components/alert.js
+++ src/js/components/alert.js
@@ -1,40 +1,19 @@
 var Alert = (function() {
 
-    /**
-     * Set up locally-scoped variables
-     */
-
-    /**
-     * Adding both prefixed ".rvt-" and old ".alert"  versions of the
-     * selectors here. Let's eventually look at deprecating the
-     * old un-prefixed version.
-     */
-    var dismissButtons = document.querySelectorAll('.rvt-alert__dismiss, .alert__dismiss');
-
     var init = function() {
-        // Check to make sure there are dismissable alerts in the DOM.
-        if(dismissButtons.length != 0) {
-            _bindUiActions(dismissButtons);
-        }
+        _bindUiActions();
     }
 
-    /**
-     * @param {object} els - HTML elements to bind the actions to.
-     */
-    var _bindUiActions = function(els) {
-        for (var i = 0; i < els.length; i++) {
-            els[i].addEventListener('click', function() {
-                dismissAlert(this);
-            });
-        }
+    var _bindUiActions = function() {
+        document.addEventListener('click', function(e) {
+            dismissAlert(e);
+        });
     }
 
-    /**
-     * @param {object} context - the HTML element that was clicked to
-     * to dimiss the alert
-     */
-    var dismissAlert = function(context) {
-        var elToDismiss = context.parentNode;
+    var dismissAlert = function(e) {
+        var dismissButton = e.target.closest('.rvt-alert__dismiss');
+        if (!dismissButton) return;
+        var elToDismiss = dismissButton.parentNode;
         elToDismiss.parentNode.removeChild(elToDismiss);
     }
 
