e8c4b7eaaec732985aef376723d7653fa0f74d02
lmcgrana <lmcgrana@iu.edu>
2018-02-16 14:37:26 -0500

    Adds a few comments for clarity

--- src/js/components/alert.js
+++ src/js/components/alert.js
@@ -12,8 +12,11 @@ var Alert = (function() {
 
     var dismissAlert = function(e) {
         var dismissButton = e.target.closest('.rvt-alert__dismiss');
+        // If the target wasn't the dismiss button bail.
         if (!dismissButton) return;
+        // Get the parent node of the dsimiss button i.e. the alert container
         var elToDismiss = dismissButton.parentNode;
+        // Go up one level to the parent of the alert and then remove the alert
         elToDismiss.parentNode.removeChild(elToDismiss);
     }
 
