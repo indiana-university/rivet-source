2a4e0e8d079c94059ad78d5a6da58bd2856c1bca
Josh LaMar <jolamar@iu.edu>
2018-08-15 14:03:45 -0600

    adds a few unit tests for tabs

--- tests/unit/tabs.js
+++ tests/unit/tabs.js
@@ -11,6 +11,7 @@ const TEMPLATE = fs.readFileSync('src/components/20-tabs/tabs.hbs');
 // `aria` prefixed attributes
 const SELECTED = 'aria-selected';
 const HIDDEN   = 'hidden';
+const TABINDEX   = 'tabindex';
 
 describe('Tabs component behavior', function () {
     const body = document.body;
@@ -31,7 +32,30 @@ describe('Tabs component behavior', function () {
             assert.equal(panels[0].getAttribute(HIDDEN), null);
         });
         it('The second panel is closed', function () {
-            assert.equal(panels[1].getAttribute(HIDDEN), "");
+            assert.equal(panels[1].getAttribute(HIDDEN), "hidden");
+        });
+    });
+
+    describe('Can activate tab', function () {
+        it('Activates the third tab', function () {
+            Tabs.activateTab('tab-3');
+            // active panels should not have the hidden attribute
+            assert.equal(panels[2].getAttribute(HIDDEN), null);
+
+            // deactive panels should have the hidden attribute
+            assert.equal(panels[0].getAttribute(HIDDEN), 'hidden');
+
+            // the active tab should have the aria-selected attribute set to true
+            assert.equal(tabs[2].getAttribute(SELECTED), 'true');
+
+            // the deactivated tab should have the aria-selected attribute set to false
+            assert.equal(tabs[0].getAttribute(SELECTED), 'false');
+
+            // active tabs should not have a tabindex
+            assert.equal(tabs[2].getAttribute(TABINDEX), null);
+
+            // deactive tabs have -1 tabindex
+            assert.equal(tabs[0].getAttribute(TABINDEX), -1);
         });
     });
 
