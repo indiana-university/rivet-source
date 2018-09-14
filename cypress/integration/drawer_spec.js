076f769b14f652635dc8773e9e853064cdecee8b
Josh LaMar <jolamar@iu.edu>
2018-09-14 11:49:18 -0600

    updates drawer tests

--- cypress/integration/drawer_spec.js
+++ cypress/integration/drawer_spec.js
@@ -1,20 +1,31 @@
-describe('Rivet Drawer', function() {
+const DRAWER_TOGGLE = '[data-drawer-toggle="mobile-drawer"]';
+const DRAWER_MENU = '#mobile-drawer';
+const DRAWER_CLOSE = '.rvt-drawer__bottom-close'
+const DEV_SERVER = 'http://localhost:3000';
+
+describe('Rivet drawer interactions', function() {
     it('Visits the drawer page', function() {
-        cy.visit('http://localhost:3000/components/preview/header--persistent-drawer')
+        cy.visit(DEV_SERVER + '/components/preview/header--persistent-drawer')
     })
 
     it('Should see a button', function() {
-        cy.get('.rvt-drawer-button')
+        cy.get(DRAWER_TOGGLE)
             .should('have.attr', 'data-drawer-toggle', 'mobile-drawer')
     })
 
+    it('Should not see a drawer menu', function() {
+        cy.get(DRAWER_MENU)
+            .should('not.be.visible')
+    })
+
     it('Should be able to open the drawer', function() {
-        cy.get('.rvt-drawer-button')
+        cy.get(DRAWER_TOGGLE)
             .click()
             .should('have.attr', 'aria-expanded', 'true')
 
-        cy.get('#mobile-drawer')
+        cy.get(DRAWER_MENU)
             .should('have.attr', 'aria-hidden', 'false')
+            .and('be.visible')
     })
 
     it('Should be able to use arrow keys', function() {
@@ -38,11 +49,29 @@ describe('Rivet Drawer', function() {
             .trigger('keyup', {keyCode: 27, which: 27})
 
 
-        cy.get('.rvt-drawer-button')
+        cy.get(DRAWER_TOGGLE)
             .click()
             .should('have.attr', 'aria-expanded', 'false')
 
-        cy.get('#mobile-drawer')
+        cy.get(DRAWER_MENU)
             .should('have.attr', 'aria-hidden', 'true')
+            .and('not.be.visible')
+    })
+
+    it('Should be able to close with close button', function() {
+        cy.get(DRAWER_TOGGLE)
+            .click()
+
+        // down arrow -> up arrow -> click
+        cy.focused()
+            .trigger('keydown', {keyCode: 40, which: 40})
+        cy.focused()
+            .trigger('keydown', {keyCode: 38, which: 38})
+        cy.focused()
+            .click()
+
+        cy.get(DRAWER_MENU)
+            .should('have.attr', 'aria-hidden', 'true')
+            .and('not.be.visible')
     })
 })
