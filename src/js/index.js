5f9f4a26c4bd0346af301b154f8d5967186a28b4
lmcgrana <lmcgrana@iu.edu>
2018-12-14 14:58:14 -0500

    Fixes indentation

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
 (function() {
-    /**
-     * Kick off all components
-     */
-    Alert.init();
-    Drawer.init();
-    Dropdown.init();
-    Modal.init();
-    Tabs.init();
-    FileInput.init();
+  /**
+   * Kick off all components
+   */
+  Alert.init();
+  Drawer.init();
+  Dropdown.init();
+  Modal.init();
+  Tabs.init();
+  FileInput.init();
 })();
 /* eslint-enable */
