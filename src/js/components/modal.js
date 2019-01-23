c54ba08f3d18f9b68f90d63b6dd7ace7689ec17a
lmcgrana <lmcgrana@iu.edu>
2019-01-23 10:15:15 -0500

    Updates customEvent emitter to always be the modal's id attribute

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -84,23 +84,10 @@ var Modal = (function() {
     document.body.classList.add('rvt-modal-open');
     
     /**
-     * TODO: Here we are checking to see if the modal was triggered by
-     * clicking an element with the "data-modal-trigger" attribute. This
-     * is to cover the case that someone might use a modal without a
-     * trigger associated with it i.e. opening the modal programmatically
-     * via the API. This could probably be refactored to always use the
-     * modal's "id" attribute when emitting the custom event. Same for the
-     * ".close()" method below.
+     * Emit a custom 'modalOpen' event and send along the modal's
+     * id attribute in the event.detail.name()
      */
-    
-    // If there is a corresponding modal trigger
-    if (activeTrigger !== null) {
-      // Fire the custom event from the trigger
-      fireCustomEvent(activeTrigger, TRIGGER_ATTR, 'modalOpen');
-    } else {
-      // Otherwise use the modal's id attribute
-      fireCustomEvent(activeModal, 'id', 'modalOpen');
-    }
+    fireCustomEvent(activeModal, 'id', 'modalOpen');
 
     if (callback && typeof callback === 'function') {
       callback();
@@ -137,21 +124,10 @@ var Modal = (function() {
     document.body.classList.remove('rvt-modal-open');
     
     /**
-     * TODO: Here we are checking to see if the modal was triggered by
-     * clicking an element with the "data-modal-trigger" attribute. This
-     * is to cover the case that someone might use a modal without a
-     * trigger associated with it i.e. opening the modal programmatically
-     * via the API. This could probably be refactored to always use the
-     * modal's "id" attribute when emitting the custom event. Same for the
-     * ".open()" method above.
+     * Emit a custom 'modalClose' event and send along the modal's
+     * id attribute in the event.detail.name()
      */
-    
-    // If there is a corresponding modal trigger
-    if (activeTrigger !== null) {
-      fireCustomEvent(activeTrigger, TRIGGER_ATTR, 'modalClose');
-    } else {
-      fireCustomEvent(activeModal, 'id', 'modalClose');
-    }
+    fireCustomEvent(activeModal, 'id', 'modalClose');
 
     if (callback && typeof callback === 'function') {
         callback();
