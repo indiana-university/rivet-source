a38031ae1ff966157528c7a07cff5626fafd8d56
Rachel <racmocon@gmail.com>
2019-04-11 10:38:41 -0400

    Changed from npx cypress to global, and edited alert_spec test

--- cypress/integration/alert_spec.js
+++ cypress/integration/alert_spec.js
@@ -1,4 +1,6 @@
 const ALERT_INFO = '[aria-labelledby="information-alert-title"]';
+const ALERT_WARNING = '[aria-labelledby="warning-alert-title"]';
+const ALERT_SUCCESS = '[aria-labelledby="success-alert-title"]';
 const ALERT_CLOSE = ALERT_INFO + '>.rvt-alert__dismiss';
 const DEV_SERVER = "http://localhost:3000";
 
@@ -18,7 +20,7 @@ describe('Rivet alert interactions', function() {
   it('Should be able to close the alert', function() {
     cy.get(ALERT_CLOSE).click();
 
-    cy.get(ALERT_INFO).should('not.be.visible');
+    cy.get(ALERT_INFO).should('not.exist');
   });
 
   it('Should be able to dismiss with .dismiss() method', function() {
@@ -26,7 +28,7 @@ describe('Rivet alert interactions', function() {
       win.Alert.dismiss('warning-alert-title');
     });
 
-    cy.get(ALERT_INFO).should('not.exist');
+    cy.get(ALERT_WARNING).should('not.exist');
   });
 
   it('Should be able to dismiss with .dismiss() method with DOM element', function() {
@@ -37,6 +39,6 @@ describe('Rivet alert interactions', function() {
       win.Alert.dismiss(alert);
     });
 
-    cy.get(ALERT_INFO).should('not.exist');
+    cy.get(ALERT_SUCCESS).should('not.exist');
   });
 });
