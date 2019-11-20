374dd855a4922b0c08e9edd239c2087d0e4bc02e
lmcgrana <lmcgrana@iu.edu>
2019-11-20 12:10:00 -0500

    Imported polyfills needed for IE11

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,11 @@
  * Copyright (C) 2018 The Trustees of Indiana University
  * SPDX-License-Identifier: BSD-3-Clause
  */
+// Polyfills
+import './polyfills/closest';
+import './polyfills/CustomEvent';
 
+// Components
 import Alert from './components/alert';
 import Sidenav from './components/sidenav';
 
