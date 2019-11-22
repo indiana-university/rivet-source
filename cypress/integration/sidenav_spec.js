1d68711b3279e414fed5e6c682ba784110dd0b1b
lmcgrana <lmcgrana@iu.edu>
2019-11-22 09:10:51 -0500

    Refactored element references into beforeEach function

--- cypress/integration/sidenav_spec.js
+++ cypress/integration/sidenav_spec.js
@@ -1,17 +1,21 @@
 const DEV_SERVER = 'http://localhost:3000';
 
+beforeEach(function() {
+  cy.visit(`${DEV_SERVER}/components/preview/sidenav`);
+  /**
+   * Use cypress aliases to share the context of the list and toggle
+   * elements across different assertions.
+   */
+  cy.get('[data-sidenav-list="toggle-1"]').as('list');
+  cy.get('[data-sidenav-toggle="toggle-1"]').as('toggle');
+});
+
 describe('Sidenav Interaction', function() {
-  it('Visits the Sidenav page', function() {
-    cy.visit(`${DEV_SERVER}/components/preview/sidenav`);
-  });
-  
   it('Should toggle visibility of a nested list', function() {
-    cy.get('[data-sidenav-list="toggle-1"]')
-      .as('list')
+    cy.get('@list')
       .should('be.hidden');
       
-    cy.get('[data-sidenav-toggle="toggle-1"]')
-      .as('toggle')
+    cy.get('@toggle')
       .should('have.attr', 'aria-expanded', 'false')
       .and('have.attr', 'aria-haspopup', 'true')
       .click()
