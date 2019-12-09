28596112c065620963fb44e7ea62eeff0d348030
Zachary Todd <zttodd@iu.edu>
2019-12-09 15:17:17 -0500

    Set up empty ES6 class with methods for Dropdown component

---
+++ src/js/components/dropdown.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2018 The Trustees of Indiana University
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+export default class Dropdown {
+  constructor(element) {
+    this.element = element;
+
+    this.init();
+  }
+
+  openMenu() {
+
+  }
+
+  closeMenu() {
+
+  }
+
+  toggle() {
+
+  }
+
+  _setUpMenu() {
+
+  }
+
+  _handleClick() {
+
+  }
+
+  _handleKeydown() {
+
+  }
+
+  init() {
+
+  }
+
+  destroy() {
+
+  }
+}
