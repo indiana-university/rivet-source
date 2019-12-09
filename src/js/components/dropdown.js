ed3baab7b633374b38888afdfdff948ad0b4d2cd
Zachary Todd <zttodd@iu.edu>
2019-12-09 15:25:26 -0500

    Added init/destroy listeners and selectors

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -6,6 +6,12 @@
 export default class Dropdown {
   constructor(element) {
     this.element = element;
+    this.focusableElements = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]';
+    this.toggleAttribute = '[data-dropdown-toggle]';
+    this.menuSelector = '[data-dropdown-menu]';
+
+    // Bind methods
+    this._handleClick = this._handleClick.bind(this);
 
     this.init();
   }
@@ -35,10 +41,10 @@ export default class Dropdown {
   }
 
   init() {
-
+    this.element.addEventListener('click', this._handleClick, false);
   }
 
   destroy() {
-
+    this.element.removeEventListener('click', this._handleClick, false);
   }
 }
