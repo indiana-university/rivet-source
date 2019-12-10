5561d0f13061859c600a917fef571964aec75d06
Zachary Todd <zttodd@iu.edu>
2019-12-10 10:28:42 -0500

    Refactored open/close on click handler

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -19,9 +19,8 @@ export default class Dropdown {
     // Keyboard Codes
     this.keys = keyCodes;
 
-    // Keeps track of the currently active toggle and helps with focus management
-    this.activeToggle = null;
-    this.activeMenu = null;
+    // Keeps track of the currently active dropdown and helps with focus management
+    this.activeDropdown = null;
 
     // Bind methods
     this._handleClick = this._handleClick.bind(this);
@@ -29,15 +28,7 @@ export default class Dropdown {
     this.init();
   }
 
-  openMenu(id, callback) {
-    // If there's an open menu, close it.
-    if (this.activeMenu) {
-      this.closeMenu(this.activeMenu);
-    }
-
-    // Set the current active menu the menu we're about to open
-    this.activeMenu = id;
-
+  openMenu() {
     // Return if disabled dropdown is being opened programmatically
     if (this.toggleElement.hasAttribute('disabled')) {
       return;
@@ -46,25 +37,23 @@ export default class Dropdown {
     // If the menu was opened by clicking an associated toggle
     if (this.toggleElement && this.toggleElement !== null) {
       this.toggleElement.setAttribute('aria-expanded', 'true');
-
-      this.activeToggle = this.toggleElement;
     }
 
     // Remove the 'hidden' attribute to show the menu
     this.menuElement.setAttribute('aria-hidden', 'false');
-
-    // Execute supplied callback function if it exists
-    if (callback && typeof callback === 'function') {
-      callback();
-    }
   }
 
   closeMenu() {
+    // Return if disabled dropdown is being closed programmatically
+    if (this.toggleElement.hasAttribute('disabled')) {
+      return;
+    }
 
-  }
+    if (this.toggleElement && this.toggleElement !== undefined) {
+      this.toggleElement.setAttribute('aria-expanded', 'false');
+    }
 
-  toggle() {
-
+    this.menuElement.setAttribute('aria-hidden', 'true');
   }
 
   _setUpMenu() {
@@ -72,22 +61,26 @@ export default class Dropdown {
   }
 
   _handleClick(event) {
-    if (!this.toggleElement || this.toggleElement.getAttribute('aria-expanded') === 'true') {
-      // No menu has been opened yet and the event target was not a toggle, so bail.
-      if (!this.activeMenu) return;
+    const menu = event.target.closest(this.menuAttribute);
 
+    // Use this boolean on the event object in place of stopPropagation()
+    if (menu && menu !== null) {
+      event.clickedWithinMenu = true;
+    }
+
+    if (!this.toggleElement || this.toggleElement.getAttribute('aria-expanded') === 'true') {
       /**
        * Otherwise close the currently open menu unless the click
        * happened inside of it.
        */
       if (!event.clickedWithinMenu) {
-        this.closeMenu(this.activeMenu);
+        this.closeMenu();
       }
 
       return;
     }
 
-    var dropdownId = this.toggleElement.getAttribute(this.toggleAttribute);
+    const dropdownId = this.element;
 
     this.openMenu(dropdownId);
   }
