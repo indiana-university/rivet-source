782ec22eb7e35aa6886ceb600fcb7de56f1cb5a7
Zachary Todd <zttodd@iu.edu>
2020-01-06 11:52:51 -0500

    Added keyboard down arrow functionality

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -18,14 +18,12 @@ export default class Dropdown {
     this.menuAttribute = '[data-dropdown-menu]';
     this.menuElement = this.element.querySelector(this.menuAttribute);
 
-    // Keyboard Codes
-    this.keys = keyCodes;
-
     // Keeps track of the currently active dropdown and helps with focus management
     this.activeDropdown = null;
 
     // Bind methods
     this._handleClick = this._handleClick.bind(this);
+    this._handleKeydown = this._handleKeydown.bind(this);
 
     this.init();
   }
@@ -64,8 +62,24 @@ export default class Dropdown {
     menuList.setAttribute('aria-hidden', 'true');
   }
 
-  _setUpMenu() {
+  _setUpMenu(menu) {
+    const menuObject = {};
 
+    // Create a real Array of all the focusable elements in the menu
+    const menuFocusables = Array.prototype.slice.call(
+      menu.querySelectorAll(this.focusableElements)
+    );
+
+    // Create a property to hold an array of all focusables
+    menuObject.all = menuFocusables;
+
+    // Create a property with a reference to the first focusable
+    menuObject.first = menuFocusables[0];
+
+    // Create a property with a reference to the last focusable
+    menuObject.last = menuFocusables[menuFocusables.length - 1];
+
+    return menuObject;
   }
 
   _handleClick(event) {
@@ -103,15 +117,90 @@ export default class Dropdown {
     }
   }
 
-  _handleKeydown() {
+  _handleKeydown(event) {
+    const dropdown = event.target.closest(this.dropdownAttribute);
 
+    if (!dropdown) return;
+
+    const toggleButton = dropdown.querySelector(this.toggleAttribute);
+    const menuList = dropdown.querySelector(this.menuAttribute);
+
+    // Delegate event to only this instance of the dropdown
+    if (dropdown === this.element) {
+      switch (event.keyCode) {
+        case keyCodes.down: {
+          event.preventDefault();
+
+          const toggle = event.target.closest(this.toggleAttribute);
+
+          /**
+           * If you were focused on the dropdown toggle
+           */
+          if (toggle && toggle !== null) {
+            // If you're focused on the toggle button and the menu is open.
+            if (toggle.getAttribute('aria-expanded') === 'true') {
+              const currentMenu = this._setUpMenu(this.menuElement);
+  
+              currentMenu.first.focus();
+            }
+  
+            this.openMenu(toggleButton, menuList);
+          }
+
+          /**
+           * Handle down arrow key when inside the open menu.
+           */
+
+          if (event.target.closest(this.menuAttribute) !== null) {
+            const currentMenu = this._setUpMenu(this.menuElement);
+
+            let currentIndex;
+
+            /**
+             * This keeps track of which button/focusable is focused in the open menu
+             */
+            for (let i = 0; i < currentMenu.all.length; i++) {
+              if (event.target == currentMenu.all[i]) {
+                currentIndex = i;
+              }
+            }
+
+            const nextItem = currentMenu.all[currentIndex + 1];
+
+            if (!nextItem) {
+              currentMenu.first.focus();
+
+              return;
+            }
+
+            nextItem.focus();
+          }
+  
+          break;
+        }
+  
+        case keyCodes.up: {
+          break;
+        }
+  
+        case keyCodes.escape: {
+          break;
+        }
+  
+        case keyCodes.tab: {
+          break;
+        }
+      }
+    }
   }
 
   init() {
     document.addEventListener('click', this._handleClick, false);
+    document.addEventListener('keydown', this._handleKeydown, false);
   }
 
   destroy() {
     document.removeEventListener('click', this._handleClick, false);
+    document.removeEventListener('keydown', this._handleKeydown, false);
   }
 }
