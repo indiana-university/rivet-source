d1d61e84140e5d869c71c1d006dc6b4c7c84eb34
Rachel <racmocon@gmail.com>
2020-01-09 15:10:25 -0500

    Refactored code to reflect feedback

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -14,26 +14,24 @@ export default class Tabs {
     this.tablist = this.element.querySelector('[role="tablist"]');
     this.tabAttribute = 'data-tab';
     this.tabSelector = `[${this.tabAttribute}]`;
-    this.tabs = this.element.querySelectorAll(this.tabSelector);
+    this.tabs = nodeListToArray(this.element.querySelectorAll(this.tabSelector));
     this.panelAttribute = 'data-tab-panel';
     this.panelSelector = `[${this.panelAttribute}]`;
     this.panels = nodeListToArray(this.element.querySelectorAll(this.panelSelector));
 
 
     // Determine if a specific panel has been initialized with the data-tab-init attribute, otherwise, use the first tab
-    let initialPanel = {}
-    this.panels.map((panel, index) => {
-      if (this.panels[index].hasAttribute('data-tab-init') === true) {
-        initialPanel = this.element.querySelector(`[${this.panelAttribute}="${panel.dataset.tabPanel}"]`);
+    let initialPanel;
+    this.panels.forEach((panel, index) => {
+      if (panel.hasAttribute('data-tab-init')) {
+        initialPanel = panel;
       } else {
-        const currentTab = this.element.querySelector(`[${this.tabAttribute}="${panel.dataset.tabPanel}"]`);
-
-        currentTab.setAttribute('tabindex', '-1');
+        this.tabs[index].setAttribute('tabindex', '-1');
       }
     })
 
     // If a specific panel was initialized set this.openOnInit equal to it, otherwise fallback to the first panel
-    initialPanel ? this.openOnInit = initialPanel : this.openOnInit = this.element.querySelector(`[${this.panelAttribute}="${this.panels[0].getAttribute(this.panelAttribute)}"]`);
+    this.openOnInit = initialPanel || this.panels[0];
 
     // bind methods
     this._handleClick = this._handleClick.bind(this);
@@ -82,44 +80,43 @@ export default class Tabs {
     if (!currentTab) return;
 
     // Create an array of all the focusable elements within the modal
-    const focusList = nodeListToArray(this.tabs);
-    const nextTab = focusList.indexOf(currentTab) + 1;
-    const prevTab = focusList.indexOf(currentTab) - 1;
+    const nextTab = this.tabs.indexOf(currentTab) + 1;
+    const prevTab = this.tabs.indexOf(currentTab) - 1;
 
     switch (event.keyCode) {
       case keyCodes.right || keyCodes.down: {
 
-        !focusList[nextTab] ? focusList[0].focus() : focusList[nextTab].focus();
+        !this.tabs[nextTab] ? this.tabs[0].focus() : this.tabs[nextTab].focus();
 
         break;
       }
       case keyCodes.down: {
 
-        !focusList[nextTab] ? focusList[0].focus() : focusList[nextTab].focus();
+        !this.tabs[nextTab] ? this.tabs[0].focus() : this.tabs[nextTab].focus();
 
         break;
       }
       case keyCodes.left: {
 
-        !focusList[prevTab] ? focusList[focusList.length - 1].focus() : focusList[prevTab].focus();
+        !this.tabs[prevTab] ? this.tabs[this.tabs.length - 1].focus() : this.tabs[prevTab].focus();
 
         break;
       }
       case keyCodes.up: {
 
-        !focusList[prevTab] ? focusList[focusList.length - 1].focus() : focusList[prevTab].focus();
+        !this.tabs[prevTab] ? this.tabs[this.tabs.length - 1].focus() : this.tabs[prevTab].focus();
 
         break;
       }
       case keyCodes.end: {
 
-        focusList[focusList.length - 1].focus();
+        this.tabs[this.tabs.length - 1].focus();
 
         break;
       }
       case keyCodes.home: {
 
-        focusList[0].focus();
+        this.tabs[0].focus();
 
         break;
       }
@@ -132,7 +129,7 @@ export default class Tabs {
   /**
    * This function is used to trigger the 'tabActivated' custom event,
    * deactivate any other tab, and activate a tab/panel pair. It removes the
-   * panel's hidden attribute, adds the .rvt-tab-open class, sets the tab's
+   * panel's hidden attribute, sets the tab's
    * aria-selected attribute to true. It also allows developers to setup a
    * custom callback function.
    * @param {Function} callback 
@@ -152,15 +149,17 @@ export default class Tabs {
 
     const trigger = this.element.querySelector(`[${this.tabAttribute}="${tab.dataset.tabPanel}"]`);
 
-    this.panels.forEach(panel => {
+    this.panels.forEach((panel, index) => {
       if (panel.getAttribute(this.panelAttribute) !== tab.dataset.tabPanel && !panel.hasAttribute('hidden')) {
-        this.deactivateTab(panel);
+        // Deactivate the appropriate tab/panel pair
+        panel.setAttribute('hidden', '');
+        this.tabs[index].setAttribute('aria-selected', 'false');
+        this.tabs[index].setAttribute('tabindex', '-1');
       }
     });
 
     // Activate the appropriate tab/panel pair
     tab.removeAttribute('hidden');
-    tab.classList.add('rvt-tab-open');
     trigger.setAttribute('aria-selected', 'true');
     trigger.removeAttribute('tabindex');
 
@@ -169,41 +168,8 @@ export default class Tabs {
     }
   }
 
-  /**
-   * This function is used to trigger the 'tabDeactivated' custom event,
-   * and deactivates a tab/panel pair. It adds the hidden attribute, removes
-   * the .rvt-tab-open class, and sets the tab's aria-selected attribute to
-   * false. It also allows developers to setup a custom callback function.
-   * @param {Function} callback 
-   */
-  deactivateTab(tab, callback) {
-    // Trigger tabDeactivated custom event. This event is used to control the process of switching between tabs.
-
-    const deactivationEvent = dispatchCustomEvent(
-      'tabDeactivated',
-      tab,
-      {
-        id: tab.dataset.tabPanel,
-      }
-    );
-
-    if (!deactivationEvent) return;
-
-    const trigger = this.element.querySelector(`[${this.tabAttribute}="${tab.dataset.tabPanel}"]`);
-
-    // Deactivate the appropriate tab/panel pair
-    tab.setAttribute('hidden', '');
-    tab.classList.remove('rvt-tab-open');
-    trigger.setAttribute('aria-selected', 'false');
-    trigger.setAttribute('tabindex', '-1');
-
-    if (callback && typeof callback === 'function') {
-      callback();
-    }
-  }
-
   init() {
-    if (this.openOnInit) { this.activateTab(this.openOnInit); }
+    this.activateTab(this.openOnInit);
 
     this.destroy();
 
