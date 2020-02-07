c852309cee0b51ab14b61abc2e468379c82e59ba
Zachary Todd <zttodd@iu.edu>
2020-02-07 09:38:38 -0500

    Renamed .tokensconfig.json to .tokens.config.json

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const rollup = require('rollup');
 const sass = require('gulp-sass');
 const strip = require('gulp-strip-comments');
 const StyleDictionary = require('style-dictionary').extend(
-  './.tokensconfig.json'
+  './.tokens.config.json'
 );
 const stylelint = require('gulp-stylelint');
 const uglify = require('gulp-uglify');
