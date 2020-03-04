a4ebea532fd350b6d7872a6a5407a92e1319f5c2
lmcgrana <lmcgrana@iu.edu>
2020-03-04 17:18:59 -0500

    Updated aliases to point at new example page

--- cypress/integration/accordion_spec.js
+++ cypress/integration/accordion_spec.js
@@ -1,18 +1,11 @@
 const DEV_SERVER = 'http://localhost:3000';
 
 beforeEach(function () {
-  cy.visit(`${DEV_SERVER}/components/preview/accordion`);
   cy.visit(`${DEV_SERVER}/components/preview/example-layouts--accordion-test`);
   /**
    * Use cypress aliases to share the context of the accordion trigger and panel
    * elements across different assertions.
    */
-  cy.get('[data-accordion-panel="test-accordion-1"]').as('panel1');
-  cy.get('[data-accordion-trigger="test-accordion-1"]').as('trigger1');
-  cy.get('[data-accordion-panel="test-accordion-2"]').as('panel2');
-  cy.get('[data-accordion-trigger="test-accordion-2"]').as('trigger2');
-  cy.get('[data-accordion-panel="test-accordion-3"]').as('panel3');
-  cy.get('[data-accordion-trigger="test-accordion-3"]').as('trigger3');
   cy.get('[data-accordion-panel="my-new-accordion-1"]').as('panel1');
   cy.get('[data-accordion-trigger="my-new-accordion-1"]').as('trigger1');
   cy.get('[data-accordion-panel="my-new-accordion-2"]').as('panel2');
@@ -25,42 +18,26 @@ beforeEach(function () {
 
 describe('Rivet accordion interactions', function () {
   it('Should see the accordion page', function () {
-    cy.get('@panel3')
+    cy.get('@panel1')
       .should('be.visible')
       .should('not.have.attr', 'hidden');
 
     cy.get('@panel1').should('not.be.visible');
     cy.get('@panel2').should('not.be.visible');
+    cy.get('@panel3').should('not.be.visible');
+    cy.get('@panel4').should('not.be.visible');
   });
 
   it('Should be able to open accordion panels', function () {
-    // Close accordion-3
-    cy.get('@trigger1').click();
-
+    cy.get('@trigger2').click();
     cy.get('@panel1').should('be.visible');
-    cy.get('@panel2').should('not.be.visible');
-    cy.get('@panel3').should('be.visible');
-  });
-
-  it('Should be able to close accordion panels', function () {
-    // Close accordion-3
-    cy.get('@trigger3').click();
-
-    cy.get('@panel1').should('not.be.visible');
-    cy.get('@panel2').should('not.be.visible');
+    cy.get('@panel2').should('be.visible');
     cy.get('@panel3').should('not.be.visible');
   });
 
-  // TODO: re-write these tests and improve coverage.
+  it('Should be able to close accordion panels', function () {
+    // Close first that was opened via data attribute
+    cy.get('@trigger1').click();
+    cy.get('@panel1').should('not.be.visible');
+  });
   
-  // it('Should be able to navigate with the keyboard', function () {
-  //   cy.get('@accordion3').trigger('keyup', { keyCode: 38 });
-  //   cy.focused().should('be', '@accordion2');
-
-  //   // Ensure that keydown from an open accordion (with focusable content) still moves to next accordion
-  //   cy.get('@trigger2').click();
-  //   cy.get('@accordion2').trigger('keydown', { keyCode: 40 });
-  //   cy.focused().should('be', '@accordion3');
-
-  // });
-});
