3ee11ee72c67b0a7083cc161cc1d7a9063e8cd24
Scott Anthony Murray <scanmurr@iu.edu>
2021-03-29 10:52:30 -0400

    Converted alert component to wicked element

--- src/js/components/alert.js
+++ src/js/components/alert.js
@@ -2,55 +2,57 @@
  * Copyright (C) 2018 The Trustees of Indiana University
  * SPDX-License-Identifier: BSD-3-Clause
  */
-import dispatchCustomEvent from '../utilities/dispatchCustomEvent';
-import { isNode } from '../utilities/domHelpers';
+import Component from './component';
 
-export default class Alert {
-  constructor(element) {
-    // Instance properties
-    this.element = element;
-    this.closeAttribute = 'data-alert-close';
-    this.closeSelector = `[${this.closeAttribute}]`;
-    this.closeButton = null;
+export default class Alert extends Component {
+  static get selector() {
+    return '[data-alert]';
+  }
 
-    // bind methods
-    this._handleClick = this._handleClick.bind(this);
+  static get methods() {
+    return {
+      init() {
+        console.log('Alert::init()');
 
-    // Check to make sure that a DOM element was passed in for initialization
-    if (!isNode(this.element)) {
-      throw new TypeError(
-        'A DOM element should be passed as the first argument to initialize the alert'
-      );
+        this.closeAttribute = 'data-alert-close';
+        this.closeSelector = `[${this.closeAttribute}]`;
+        this.closeButton = null;
+
+        Component.bindMethodToDOMElement(this.element, 'dismiss', this.dismiss);
+        
+        this._handleClick = this._handleClick.bind(this);
+      },
+
+      connected() {
+        console.log('Alert::connected()');
+
+        this.closeButton = this.element.querySelector(this.closeSelector);
+        this.closeButton.addEventListener('click', this._handleClick, false);
+      },
+
+      disconnected() {
+        console.log('Alert::disconnected()');
+        
+        this.closeButton.addEventListener('click', this._handleClick, false);
+      },
+
+      _handleClick() {
+        this.dismiss();
+      },
+    
+      dismiss() {
+        const dismissEvent = Component.dispatchCustomEvent(
+          'alertDismiss',
+          this.element,
+          {
+            id: this.element.dataset.alert
+          }
+        );
+    
+        if (!dismissEvent) return;
+    
+        this.element.remove();
+      }
     }
-
-    this.init();
-  }
-
-  _handleClick() {
-    this.dismiss();
-  }
-
-  dismiss() {
-    /**
-     * Custom event for handling removal of alerts from a page.
-     */
-    const dismissEvent = dispatchCustomEvent('alertDismiss', this.element, {
-      id: this.element.dataset.alert
-    });
-
-    if (!dismissEvent) return;
-
-    this.destroy();
-    this.element.remove();
-  }
-
-  init() {
-    // Add click handlers
-    this.closeButton = this.element.querySelector(this.closeSelector);
-    this.closeButton.addEventListener('click', this._handleClick, false);
-  }
-
-  destroy() {
-    this.closeButton.addEventListener('click', this._handleClick, false);
   }
 }
