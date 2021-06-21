ce18465ca6336cacba40c80e3c4f31c35726f3ed
Scott Anthony Murray <scanmurr@iu.edu>
2021-06-21 10:35:06 -0400

    Added disclosure tests

---
+++ cypress/integration/disclosure_spec.js
@@ -0,0 +1,110 @@
+const DISCLOSURE = '[data-rvt-disclosure]';
+const DISCLOSURE_TOGGLE = '[data-rvt-disclosure-toggle]';
+const DISCLOSURE_TARGET = '[data-rvt-disclosure-target]';
+const DEV_SERVER = "http://localhost:3000";
+const ENTER = 13;
+const ESC = 27;
+
+describe('Disclosure Interaction', function() {
+  it('Visits the disclosure page', function() {
+    cy.visit(DEV_SERVER + '/components/preview/disclosure');
+  });
+
+  it('Should see the disclosure toggle', function() {
+    cy.get(DISCLOSURE_TOGGLE).should('have.attr', 'aria-expanded', 'false');
+
+    cy.get(DISCLOSURE_TARGET)
+      .should('have.attr', 'hidden')
+      .and('not.be.visible');
+  });
+
+  it('Should be able to open the disclosure', function() {
+    cy.get(DISCLOSURE_TOGGLE)
+      .click()
+      .should('have.attr', 'aria-expanded', 'true');
+
+    cy.get(DISCLOSURE_TARGET)
+      .should('be.visible')
+      .and('not.have.attr', 'hidden');
+  });
+
+  it('Should be able to close the disclosure', function() {
+    cy.get(DISCLOSURE_TOGGLE)
+      .click()
+      .should('have.attr', 'aria-expanded', 'false');
+
+    cy.get(DISCLOSURE_TARGET)
+      .should('not.be.visible')
+      .and('have.attr', 'hidden');
+  });
+
+  it('Should be able to close with esc key', function() {
+    cy.get(DISCLOSURE_TOGGLE).click();
+
+    cy.focused().trigger('keydown', { keyCode: ESC, which: ESC });
+
+    cy.get(DISCLOSURE_TARGET).should('not.be.visible');
+  });
+
+  it('Should be able to open with .open() method', function() {
+    cy.window().then(win => {
+      var disclosure = win.document.querySelector(DISCLOSURE);
+      disclosure.open();
+    });
+
+    cy.get(DISCLOSURE_TOGGLE).should('have.attr', 'aria-expanded', 'true');
+
+    cy.get(DISCLOSURE_TARGET)
+      .should('be.visible')
+      .and('not.have.attr', 'hidden');
+  });
+
+  it('Should be able to close with .close() method', function() {
+    cy.window().then(win => {
+      var disclosure = win.document.querySelector(DISCLOSURE);
+      disclosure.close();
+    });
+
+    cy.get(DISCLOSURE_TOGGLE).should('have.attr', 'aria-expanded', 'false');
+
+    cy.get(DISCLOSURE_TARGET)
+      .should('have.attr', 'hidden')
+      .and('not.be.visible');
+  });
+
+  it('Should fire a rvt:disclosureOpened custom event with correct element references', function() {
+    cy.window().then(win => {
+      var disclosure = win.document.querySelector(DISCLOSURE);
+      var eventFired = false;
+      var eventDisclosureReference;
+      
+      win.addEventListener('rvt:disclosureOpened', event => {
+        eventFired = true;
+        eventDisclosureReference = event.target == disclosure;
+      });
+      
+      disclosure.open();
+      
+      if (!eventFired) throw new Error('Did not fire disclosureOpened event');
+      if (!eventDisclosureReference) throw new Error('Did not pass correct reference to emitting disclosure component element');
+    });
+  });
+
+  it('Should fire a rvt:disclosureClosed custom event with correct element references', function() {
+    cy.window().then(win => {
+      var disclosure = win.document.querySelector(DISCLOSURE);
+      var eventFired = false;
+      var eventDisclosureReference;
+      
+      win.addEventListener('rvt:disclosureClosed', event => {
+        eventFired = true;
+        eventDisclosureReference = event.target == disclosure;
+      });
+      
+      disclosure.close();
+      
+      if (!eventFired) throw new Error('Did not fire disclosureClosed event');
+      if (!eventDisclosureReference) throw new Error('Did not pass correct reference to emitting disclosure component element');
+    });
+  });
+});
