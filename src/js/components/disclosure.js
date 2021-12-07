dc77aacdbe164f3138fca911ff634b41e7cfd519
Scott Anthony Murray <scanmurr@iu.edu>
2021-12-07 15:51:24 -0500

    Refactored Disclosure component code
    Refactored Disclosure component code (wip)
    
    Performed third refactoring pass on Disclosure component code (wip)

--- src/js/components/disclosure.js
+++ src/js/components/disclosure.js
@@ -9,148 +9,117 @@ export default class Disclosure extends Component {
   static get methods() {
     return {
       init() {
-        console.log('Disclosure::init');
-
-        this.toggleElement = this.element.querySelector('[data-rvt-disclosure-toggle]');
-        this.targetElement = this.element.querySelector('[data-rvt-disclosure-target]');
-        this.isOpen = false;
-        this.activeToggle = null;
-        this.activeDisclosure = null;
-        
-        const icon = this.element.querySelector('svg');
-        if (icon) {
-          icon.setAttribute('focusable', 'false');
-        }
+        this._initElements();
+        this._initProperties();
+        this._removeIconFromTabOrder();
         
         Component.bindMethodToDOMElement(this, 'open', this.open);
         Component.bindMethodToDOMElement(this, 'close', this.close);
+      },
 
-        this._handleClick = this._handleClick.bind(this);
-        this._handleKeydown = this._handleKeydown.bind(this);
+      _initElements() {
+        this.toggleElement = this.element.querySelector('[data-rvt-disclosure-toggle]');
+        this.targetElement = this.element.querySelector('[data-rvt-disclosure-target]');
+      },
+
+      _initProperties() {
+        this.isOpen = false;
+        this.activeToggle = null;
+        this.activeDisclosure = null;
+      },
+
+      _removeIconFromTabOrder() {
+        const icon = this.element.querySelector('svg');
+        
+        if (icon)
+          icon.setAttribute('focusable', 'false');
       },
 
       connected() {
         Component.dispatchComponentAddedEvent(this.element);
-        
-        document.addEventListener('click', this._handleClick, false);
-        document.addEventListener('keydown', this._handleKeydown, false);
       },
 
       disconnected() {
         Component.dispatchComponentRemovedEvent(this.element);
-
-        document.removeEventListener('click', this._handleClick, false);
-        document.removeEventListener('keydown', this._handleKeydown, false);
       },
 
       open() {
-        // Return if disabled disclosure is being opened programmatically
+        if (this._isDisabled())
+          return
 
-        if (this.toggleElement.hasAttribute('disabled')) {
-          return;
-        }
+        if ( ! this._eventDispatched('disclosureOpened'))
+          return
 
-        // Fire a disclosureOpened event
+        this._setOpenState()
+      },
 
-        const openEvent = Component.dispatchCustomEvent(
-          'disclosureOpened',
-          this.element
-        );
+      _isDisabled() {
+        return this.toggleElement.hasAttribute('disabled')
+      },
 
-        // Bail if the event was suppressed
-
-        if (!openEvent) return;
-
-        // Set the disclosure's open state to "true"
-
-        this.isOpen = true;
+      _setOpenState() {
         this.toggleElement.setAttribute('aria-expanded', 'true');
-
-        // Remove the 'hidden' attribute to show the element to disclose
-
         this.targetElement.removeAttribute('hidden');
 
-        // Set currently active toggle and disclosed element
-
+        this.isOpen = true;
         this.activeToggle = this.toggleElement;
         this.activeDisclosure = this.targetElement;
       },
 
       close() {
-        /**
-         * If there isn't a currently active disclosure, then bail so close() isn't
-         * fired multiple times.
-         */
-        if (!this.activeToggle) return;
+        if ( ! this._isOpen())
+          return
 
-        const closeEvent = Component.dispatchCustomEvent(
-          'disclosureClosed',
-          this.element
-        );
+        if ( ! this._eventDispatched('disclosureClosed'))
+          return
 
-        if (!closeEvent) return;
+        this._setClosedState();
+      },
 
-        this.isOpen = false;
+      _isOpen() {
+        return this.activeToggle && this.activeDisclosure
+      },
+
+      _setClosedState() {
         this.activeToggle.setAttribute('aria-expanded', 'false');
         this.activeDisclosure.setAttribute('hidden', '');
 
-        // Resets the state variables
+        this.isOpen = false;
         this.activeToggle = null;
         this.activeDisclosure = null;
       },
 
-      _handleClick(event) {
-        const toggle = event.target.closest('[data-rvt-disclosure-toggle]');
+      _eventDispatched(name) {
+        const dispatched = Component.dispatchCustomEvent(name, this.element)
 
-        // Did it come from inside open disclosure?
-        if (this.targetElement.contains(event.target)) return;
+        return dispatched
+      },
 
-        // If it came from outside component, close all open disclosures
-        if (!toggle && this.activeToggle !== null) {
-          this.close();
-          return;
-        }
+      onClick(event) {
+        if (this._clickOriginatedInsideDisclosureTarget(event))
+          return
 
-        // Check which toggle the click came from, and whether it's already opened
-        if (toggle !== this.toggleElement || this.isOpen) {
-          this.close();
-        } else {
-          this.open();
+        this._isOpen()
+          ? this.close()
+          : this.open()
+      },
+
+      _clickOriginatedInsideDisclosureTarget(event) {
+        return this.targetElement.contains(event.target)
+      },
+    
+      onKeydown(event) {
+        if ( ! this._keydownOriginatedInsideDisclosure(event))
+          return false
+        
+        if (event.keyCode === keyCodes.escape) {
+          this.close()
+          this.toggleElement.focus()
         }
       },
 
-      _shouldHandleKeydown(event) {
-        // If the keydown didn't come from within disclosure component, then bail.
-        if (!this.element.contains(event.target)) return false;
-    
-        // Delegate event to only this instance of the disclosure
-        const disclosure = event.target.closest('[data-rvt-disclosure]');
-        if (disclosure !== this.element) return false;
-    
-        return true;
-      },
-    
-      _handleKeydown(event) {
-        if (!this._shouldHandleKeydown(event)) return;
-    
-        switch (event.keyCode) {
-          case keyCodes.escape: {
-            if (!this.activeToggle) return;
-    
-            // If there's an open disclosure, close it.
-            this.close();
-    
-            this.toggleElement.focus();
-    
-            /**
-             * Resets the state variables so as not to interfere with other
-             * Escape key handlers/interactions
-             */
-            this.activeToggle = null;
-    
-            break;
-          }
-        }
+      _keydownOriginatedInsideDisclosure(event) {
+        return this.element.contains(event.target)
       }
     };
   }
