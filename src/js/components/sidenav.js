c04493e79576e52a3d76364caf16c3868ca796b3
Scott Anthony Murray <scanmurr@iu.edu>
2021-12-21 15:20:56 -0500

    Refactored Sidenav component
    First pass of sidenav refactor (wip)
    
    Second pass of sidenav refactor (wip)
    
    Third pass of sidenav refactor (wip)
    
    Added JSDoc comments to Sidenav class (wip)

--- src/js/components/sidenav.js
+++ src/js/components/sidenav.js
@@ -1,152 +1,363 @@
-/**
- * Copyright (C) 2019 The Trustees of Indiana University
+/******************************************************************************
+ * Copyright (C) 2022 The Trustees of Indiana University
  * SPDX-License-Identifier: BSD-3-Clause
- */
+ *****************************************************************************/
 
-import Component from './component';
-import { nodeListToArray } from '../utilities/domHelpers';
+import Component from './component'
+
+/******************************************************************************
+ * The sidenav component can be used to add a vertical list of navigation
+ * links to a sidebar. Sidenavs can contain dropdowns that reveal nested links.
+ *
+ * @see https://v2.rivet.iu.edu/docs/components/sidenav/
+ *****************************************************************************/
 
 export default class Sidenav extends Component {
-  static get selector() {
-    return '[data-rvt-sidenav]';
+  
+  /****************************************************************************
+   * Gets the sidenav's CSS selector.
+   *
+   * @static
+   * @returns {string} The CSS selector
+   ***************************************************************************/
+
+  static get selector () {
+    return '[data-rvt-sidenav]'
   }
 
-  static get methods() {
+  /****************************************************************************
+   * Gets an object containing the methods that should be attached to the
+   * component's root DOM element. Used by wicked-elements to initialize a DOM
+   * element with Web Component-like behavior.
+   *
+   * @static
+   * @returns {Object} Object with component methods
+   ***************************************************************************/
+
+  static get methods () {
     return {
-      init() {
-        console.log('Sidenav::init');
 
-        this.toggleAttribute = 'data-rvt-sidenav-toggle';
-        this.toggleSelector = `[${this.toggleAttribute}]`;
-        this.listAttribute = 'data-rvt-sidenav-list';
-        this.listSelector = `[${this.listAttribute}]`;
-        this.openAllOnInit = this.element.hasAttribute('data-rvt-sidenav-open-all');
+      /************************************************************************
+       * Initializes the sidenav.
+       ***********************************************************************/
 
-        const menuToggles = nodeListToArray(
-          this.element.querySelectorAll(this.toggleSelector)
-        );
-        const childMenus = nodeListToArray(
-          this.element.querySelectorAll(this.listSelector)
-        );
-    
-        menuToggles.forEach((element, index) => {
-          /**
-           * Toggle button/menu list pairings
-           * element: toggle button
-           * childMenus[index]: menu list
-           */
-    
-          // Since JavaScript is available add popup semantics to toggles
-          element.setAttribute('aria-haspopup', 'true');
-    
-          // Aria-expand all toggles if everything is open on init
-          if (this.openAllOnInit) {
-            element.setAttribute('aria-expanded', 'true');
-            childMenus[index].removeAttribute('hidden');
-    
-            return;
-          }
-    
-          // Check if this toggle been manually set to expanded in markup
+      init () {
+        this._initElements()
+        this._setInitialChildMenuStates()
+
+        Component.bindMethodToDOMElement(this, 'open', this.open)
+        Component.bindMethodToDOMElement(this, 'close', this.close)
+      },
+
+      /************************************************************************
+       * Initializes sidenav child elements.
+       *
+       * @private
+       ***********************************************************************/
+
+      _initElements () {
+        this.childMenuToggleButtons = Array.from(
+          this.element.querySelectorAll('[data-rvt-sidenav-toggle]')
+        )
+
+        this.childMenus = Array.from(
+          this.element.querySelectorAll('[data-rvt-sidenav-list]')
+        )
+      },
+
+      /************************************************************************
+       * Sets the initial state of the sidenav's child menus.
+       *
+       * @private
+       ***********************************************************************/
+
+      _setInitialChildMenuStates () {
+        this._setChildMenuDefaultAriaAttributes()
+        this._shouldOpenAllChildMenus()
+          ? this._openAllChildMenus()
+          : this._setChildMenuDefaultStates()
+      },
+
+      /************************************************************************
+       * Sets the default ARIA attributes for the sidenav's child menus.
+       *
+       * @private
+       ***********************************************************************/
+
+      _setChildMenuDefaultAriaAttributes () {
+        this.childMenuToggleButtons.forEach(
+          toggleButton => toggleButton.setAttribute('aria-haspopup', 'true')
+        )
+      },
+
+      /************************************************************************
+       * Returns true if all child menus should be opened when the component
+       * is added to the DOM.
+       *
+       * @private
+       * @returns {boolean} Child menus should be opened
+       ***********************************************************************/
+
+      _shouldOpenAllChildMenus () {
+        return this.element.hasAttribute('data-rvt-sidenav-open-all')
+      },
+
+      /************************************************************************
+       * Opens all child menus.
+       *
+       * @private
+       ***********************************************************************/
+
+      _openAllChildMenus () {
+        this.childMenuToggleButtons.forEach((toggleButton, index) => {
+          toggleButton.setAttribute('aria-expanded', 'true')
+          this.childMenus[index].removeAttribute('hidden')
+        })
+      },
+
+      /************************************************************************
+       * Sets the default open/closed state for each child menu based on
+       * the ARIA attributes set by the developer.
+       *
+       * @private
+       ***********************************************************************/
+
+      _setChildMenuDefaultStates () {
+        this.childMenuToggleButtons.forEach((element, index) => {
           if (element.getAttribute('aria-expanded') === 'true') {
-            // Open list matching this toggle
-            childMenus[index].removeAttribute('hidden');
+            this.childMenus[index].removeAttribute('hidden')
           } else {
-            element.setAttribute('aria-expanded', 'false');
-            childMenus[index].setAttribute('hidden', '');
+            element.setAttribute('aria-expanded', 'false')
+            this.childMenus[index].setAttribute('hidden', '')
           }
-        });
-
-        // bind methods
-        Component.bindMethodToDOMElement(this, 'open', this.open);
-        Component.bindMethodToDOMElement(this, 'close', this.close);
-
-        this._handleClick = this._handleClick.bind(this);
+        })
       },
 
-      connected() {
-        Component.dispatchComponentAddedEvent(this.element);
+      /************************************************************************
+       * Called when the sidenav is added to the DOM.
+       ***********************************************************************/
+
+      connected () {
+        Component.dispatchComponentAddedEvent(this.element)
       },
 
-      disconnected() {
-        Component.dispatchComponentRemovedEvent(this.element);
+      /************************************************************************
+       * Called when the sidenav is removed from the DOM.
+       ***********************************************************************/
+
+      disconnected () {
+        Component.dispatchComponentRemovedEvent(this.element)
       },
 
-      onClick(event) {
-        this._handleClick(event);
+      /************************************************************************
+       * Handles click events broadcast to the sidenav.
+       *
+       * @param {Event} event - Click event
+       ***********************************************************************/
+
+      onClick (event) {
+        if (!this._clickOriginatedInChildMenuToggleButton(event)) { return }
+
+        this._setChildMenuToToggle(event)
+
+        if (!this._childMenuToToggleExists()) { return }
+
+        this._childMenuToToggleIsOpen()
+          ? this.close(this.childMenuToToggleId)
+          : this.open(this.childMenuToToggleId)
       },
 
-      _handleClick(event) {
-        const toggleButton = event.target.closest(this.toggleSelector);
-        // Exit if toggle button doesn't exist
-        if (!toggleButton) return;
-    
-        const toggleId = toggleButton.dataset.rvtSidenavToggle;
-        const targetList = this.element.querySelector(
-          `[${this.listAttribute}="${toggleId}"]`
-        );
-    
-        // Exit if the target list isn't linked with a button
-        if (!targetList || targetList.getAttribute(this.listAttribute) === '') {
-          return;
+      /************************************************************************
+       * Returns true if the given click event originated inside one of the
+       * sidenav's child menu toggle buttons.
+       *
+       * @private
+       * @param {Event} event - Click event
+       * @returns {boolean} Click originated inside child menu toggle button
+       ***********************************************************************/
+
+      _clickOriginatedInChildMenuToggleButton (event) {
+        return event.target.closest('[data-rvt-sidenav-toggle]')
+      },
+
+      /************************************************************************
+       * Sets references to the child menu to be toggled by the given click
+       * event. These references are used by other click handler submethods.
+       *
+       * @private
+       * @param {Event} event - Click event
+       ***********************************************************************/
+
+      _setChildMenuToToggle (event) {
+        this.childMenuToToggleId = event.target
+          .closest('[data-rvt-sidenav-toggle]')
+          .dataset.rvtSidenavToggle
+
+        this.childMenuToToggle = this.element.querySelector(
+          `[data-rvt-sidenav-list="${this.childMenuToToggleId}"]`
+        )
+      },
+
+      /************************************************************************
+       * Returns true if the child menu to be toggled by a click event actually
+       * exists in the DOM.
+       *
+       * @private
+       * @returns {boolean} Child menu exists
+       ***********************************************************************/
+
+      _childMenuToToggleExists () {
+        return this.childMenuToToggle &&
+               this.childMenuToToggle.getAttribute('data-rvt-sidenav-list') !== ''
+      },
+
+      /************************************************************************
+       * Returns true if the child menu to be toggled by a click event is open.
+       *
+       * @private
+       * @returns {boolean} Child menu is open
+       ***********************************************************************/
+
+      _childMenuToToggleIsOpen () {
+        return !this.childMenuToToggle.hasAttribute('hidden')
+      },
+
+      /************************************************************************
+       * Opens the child menu with the given data-rvt-sidenav-list ID value.
+       *
+       * @private
+       * @param {string} childMenuId - Child menu ID
+       ***********************************************************************/
+
+      open (childMenuId) {
+        this._setChildMenuToOpen(childMenuId)
+
+        if (!this._childMenuExists(childMenuId)) {
+          console.warn(`No such subnav child menu '${childMenuId}' in open()`)
+          return
         }
-    
-        targetList.hasAttribute('hidden')
-          ? this.open(toggleId)
-          : this.close(toggleId);
+
+        if (!this._eventDispatched('sidenavListOpened', this.childMenuToOpen)) { return }
+
+        this._openChildMenu()
       },
 
-      open(toggleId) {
-        const toggle = this.element.querySelector(
-          `[${this.toggleAttribute}="${toggleId}"]`
-        );
-        const targetList = this.element.querySelector(
-          `[${this.listAttribute}="${toggleId}"]`
-        );
+      /************************************************************************
+       * Sets references to the child menu to be opened. These references are
+       * used by other submethods.
+       *
+       * @private
+       * @param {string} childMenuId - Child menu ID
+       ***********************************************************************/
 
-        if (!toggle || !targetList) {
-          console.warn(`No such toggle '${toggleId}' in Sidenav.open()`);
-          return;
+      _setChildMenuToOpen (childMenuId) {
+        this.childMenuToOpenToggleButton = this.element.querySelector(
+          `[data-rvt-sidenav-toggle="${childMenuId}"]`
+        )
+
+        this.childMenuToOpen = this.element.querySelector(
+          `[data-rvt-sidenav-list="${childMenuId}"]`
+        )
+      },
+
+      /************************************************************************
+       * Expands the child menu to be opened.
+       *
+       * @private
+       ***********************************************************************/
+
+      _openChildMenu () {
+        this.childMenuToOpenToggleButton.setAttribute('aria-expanded', 'true')
+        this.childMenuToOpen.removeAttribute('hidden')
+      },
+
+      /************************************************************************
+       * Closes the child menu with the given data-rvt-sidenav-list ID value.
+       *
+       * @private
+       * @param {string} childMenuId - Child menu ID
+       ***********************************************************************/
+
+      close (childMenuId) {
+        this._setChildMenuToClose(childMenuId)
+
+        if (!this._childMenuExists(childMenuId)) {
+          console.warn(`No such subnav child menu '${childMenuId}' in close()`)
+          return
         }
 
-        const openEvent = Component.dispatchCustomEvent(
-          'sidenavListOpened', 
+        if (!this._eventDispatched('sidenavListClosed', this.childMenuToClose)) { return }
+
+        this._closeChildMenu()
+      },
+
+      /************************************************************************
+       * Sets references to the child menu to be closed. These references are
+       * used by other submethods.
+       *
+       * @private
+       * @param {string} childMenuId - Child menu ID
+       ***********************************************************************/
+
+      _setChildMenuToClose (childMenuId) {
+        this.childMenuToCloseToggleButton = this.element.querySelector(
+          `[data-rvt-sidenav-toggle="${childMenuId}"]`
+        )
+
+        this.childMenuToClose = this.element.querySelector(
+          `[data-rvt-sidenav-list="${childMenuId}"]`
+        )
+      },
+
+      /************************************************************************
+       * Collapses the child menu to be closed.
+       *
+       * @private
+       ***********************************************************************/
+
+      _closeChildMenu () {
+        this.childMenuToCloseToggleButton.setAttribute('aria-expanded', 'false')
+        this.childMenuToClose.setAttribute('hidden', '')
+      },
+
+      /************************************************************************
+       * Returns true if a child menu with the given ID exists.
+       *
+       * @private
+       * @returns {boolean} Child menu exists
+       ***********************************************************************/
+
+      _childMenuExists (childMenuId) {
+        const childMenuToggleButton = this.element.querySelector(
+          `[data-rvt-sidenav-toggle="${childMenuId}"]`
+        )
+
+        const childMenu = this.element.querySelector(
+          `[data-rvt-sidenav-list="${childMenuId}"]`
+        )
+
+        return childMenuToggleButton && childMenu
+      },
+
+      /************************************************************************
+       * Returns true if the custom event with the given name was successfully
+       * dispatched.
+       *
+       * @private
+       * @param {string} name - Event name
+       * @returns {boolean} Event successfully dispatched
+       ***********************************************************************/
+
+      // FIXME: Violates command-query separation and side-effects rules.
+
+      _eventDispatched (name, childMenu) {
+        const dispatched = Component.dispatchCustomEvent(
+          name,
           this.element,
-          {
-            list: targetList
-          }
-        );
-    
-        if (!openEvent) return;
-    
-        toggle.setAttribute('aria-expanded', 'true');
-        targetList.removeAttribute('hidden');
-      },
-    
-      close(toggleId) {
-        const toggle = this.element.querySelector(
-          `[${this.toggleAttribute}="${toggleId}"]`
-        );
-        const targetList = this.element.querySelector(
-          `[${this.listAttribute}="${toggleId}"]`
-        );
-        if (!toggle || !targetList) {
-          console.warn(`No such toggle '${toggleId}' in Sidenav.close()`);
-          return;
-        }
+          { list: childMenu }
+        )
 
-        const closeEvent = Component.dispatchCustomEvent(
-          'sidenavListClosed', 
-          this.element, 
-          {
-            list: targetList
-          }
-        );
-    
-        if (!closeEvent) return;
-    
-        toggle.setAttribute('aria-expanded', 'false');
-        targetList.setAttribute('hidden', '');
+        return dispatched
       }
     }
   }
