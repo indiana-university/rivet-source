86028df5c3533c3f133a54d2567c1b60dcaa400b
Scott Anthony Murray <scanmurr@iu.edu>
2021-12-27 14:02:50 -0500

    Refactored remove polyfill

--- src/js/polyfills/remove.js
+++ src/js/polyfills/remove.js
@@ -1,23 +1,27 @@
-/**
+/******************************************************************************
+ * Copyright (C) 2022 The Trustees of Indiana University
+ * SPDX-License-Identifier: BSD-3-Clause
+ *****************************************************************************/
+
+/******************************************************************************
  * ChildNode.remove() polyfill
+ *
  * https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove#Polyfill
- */
-(function(arr) {
-  arr.forEach(function(item) {
-    if (item.hasOwnProperty('remove')) {
-      return;
-    }
+ *****************************************************************************/
+
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) { return }
 
     Object.defineProperty(item, 'remove', {
       configurable: true,
       enumerable: true,
       writable: true,
-      value: function remove() {
-        if (this.parentNode === null) {
-          return;
-        }
-        this.parentNode.removeChild(this);
+      value: function remove () {
+        if (this.parentNode === null) { return }
+
+        this.parentNode.removeChild(this)
       }
-    });
-  });
-})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+    })
+  })
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype])
