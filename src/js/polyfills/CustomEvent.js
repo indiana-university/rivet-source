79f851967f4fea177fb1eac09a82d688e352a421
Scott Anthony Murray <scanmurr@iu.edu>
2021-12-27 14:16:51 -0500

    Refactored CustomEvent polyfill

--- src/js/polyfills/CustomEvent.js
+++ src/js/polyfills/CustomEvent.js
@@ -1,24 +1,33 @@
-/**
- * CustomEvent Polyfill
+/******************************************************************************
+ * Copyright (C) 2022 The Trustees of Indiana University
+ * SPDX-License-Identifier: BSD-3-Clause
+ *****************************************************************************/
+
+/******************************************************************************
+ * CustomEvent polyfill
+ *
  * https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
- */
+ *****************************************************************************/
 
-(function() {
-  if (typeof window.CustomEvent === 'function') return false;
+(function () {
+  if (typeof window.CustomEvent === 'function') { return false }
 
-  function CustomEvent(event, params) {
-    params = params || { bubbles: false, cancelable: false, detail: undefined };
-    var evt = document.createEvent('CustomEvent');
-    evt.initCustomEvent(
+  function CustomEvent (event, params) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined }
+
+    var customEvent = document.createEvent('CustomEvent')
+
+    customEvent.initCustomEvent(
       event,
       params.bubbles,
       params.cancelable,
       params.detail
-    );
-    return evt;
+    )
+
+    return customEvent
   }
 
-  CustomEvent.prototype = window.Event.prototype;
+  CustomEvent.prototype = window.Event.prototype
 
-  window.CustomEvent = CustomEvent;
-})();
+  window.CustomEvent = CustomEvent
+})()
