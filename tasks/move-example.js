0a91f9639c36412fa2d4b9f57e1f26053a19b445
Scott Anthony Murray <40362172+scottanthonymurray@users.noreply.github.com>
2022-11-07 15:01:19 -0500

    Updated example file to use latest blank layout

--- tasks/move-example.js
+++ tasks/move-example.js
@@ -11,4 +11,4 @@ const jetpack = require('fs-jetpack')
 
 console.log('Copying starter file to distribution folder...')
 
-jetpack.copy('./src/components/_extras/_index-example.html', './index.html', { overwrite: true })
+jetpack.copy('./src/index.html', './index.html', { overwrite: true })
