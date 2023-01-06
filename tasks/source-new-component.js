2b9e20bc0b994602730465eb28043081c669d7da
Scott Anthony Murray <40362172+scottanthonymurray@users.noreply.github.com>
2023-01-06 10:22:47 -0500

    Updated new-component script to generate Sass files

--- tasks/source-new-component.js
+++ tasks/source-new-component.js
@@ -6,19 +6,58 @@
 const jetpack = require('fs-jetpack')
 const { exec } = require('child_process')
 
+/******************************************************************************
+ * Should the script only generate Sass files for the new component?
+ *****************************************************************************/
+
+const sassOnly = process.argv.includes('sass-only')
+
+/******************************************************************************
+ * Extract component name and file paths.
+ *****************************************************************************/
+
 const componentName = process.argv[2].trim()
 const componentSlug = componentName.toLowerCase().replace(/\s/g, '-')
 const componentClassName = componentName.replace(/\s/g, '')
 const componentLowercaseName = componentName.toLowerCase()
-const componentPath = `./src/js/components/${componentSlug}.js`
-const componentStarterCode = jetpack
+const componentSassIndexPath = `./src/sass/${componentSlug}/_index.scss`
+const componentSassBasePath = `./src/sass/${componentSlug}/_base.scss`
+const componentJsPath = `./src/js/components/${componentSlug}.js`
+
+const componentSassStarterCode = jetpack
+                                .read('./tasks/component-starter-code.scss.txt')
+                                .replace(/{{ slug }}/gi, componentSlug)
+
+const componentJsStarterCode = jetpack
                                .read('./tasks/component-starter-code.js.txt')
                                .replace(/{{ slug }}/gi, componentSlug)
                                .replace(/{{ className }}/gi, componentClassName)
                                .replace(/{{ lowercaseName }}/gi, componentLowercaseName)
 
-jetpack.write(componentPath, componentStarterCode)
+/******************************************************************************
+ * Create Sass boilerplate files.
+ *****************************************************************************/
 
-console.log(`Wrote files for new component "${componentName}" to src/js/components/${componentSlug}.js.`)
+jetpack.write(componentSassIndexPath, `@forward 'base';`)
+console.log(`Wrote file for new component "${componentName}" to src/sass/${componentSlug}/_index.scss.`)
 
-exec(`code -r ${componentPath}`)
+jetpack.write(componentSassBasePath, componentSassStarterCode)
+console.log(`Wrote file for new component "${componentName}" to src/sass/${componentSlug}/_base.scss.`)
+
+/******************************************************************************
+ * Create JS boilerplate file.
+ *****************************************************************************/
+
+if ( ! sassOnly) {
+  jetpack.write(componentJsPath, componentJsStarterCode)
+  console.log(`Wrote file for new component "${componentName}" to src/js/components/${componentSlug}.js.`)
+}
+
+/******************************************************************************
+ * Open new files in VSCode.
+ *****************************************************************************/
+
+exec(`code -r ${componentSassBasePath}`)
+
+if ( ! sassOnly)
+  exec(`code -r ${componentJsPath}`)
