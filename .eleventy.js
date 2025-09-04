308da629fd43e2e2a622fb26d96450e6a5f1dbc8
Chris Basham <chris@bash.am>
2025-09-04 11:18:34 -0400

    Simplify 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,43 +1,16 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
-const eleventySyntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight")
-const sortCollectionByOrder = require("./src/sandbox/filters/sort-collection-by-order")
-const sortCollectionByTitle = require("./src/sandbox/filters/sort-collection-by-title")
+const eleventySyntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
-
-  /*
-  eleventyConfig.setServerOptions({
-    watch: [
-      'dist/css/*.css',
-      'dist/js/*.js'
-    ]
-  })
-
-  eleventyConfig.addPassthroughCopy('src/sandbox/css/sandbox.css')
-  eleventyConfig.addPassthroughCopy('src/sandbox/js/sandbox.js')
-  eleventyConfig.addPassthroughCopy('css/rivet.css')
-  eleventyConfig.addPassthroughCopy('js/rivet-iife.js')
-*/
-
-  // Ignore all under src except for sandbox.
-  //eleventyConfig.ignores.add('src/**');
-  //eleventyConfig.ignores.delete('src/sandbox');
-
-  eleventyConfig.addPlugin(eleventyNavigationPlugin)
-  eleventyConfig.addPlugin(eleventySyntaxHighlightPlugin)
-
-  eleventyConfig.addFilter('sortCollectionByOrder', sortCollectionByOrder)
-  eleventyConfig.addFilter('sortCollectionByTitle', sortCollectionByTitle)
-
+  eleventyConfig.addPlugin(eleventySyntaxHighlightPlugin);
+  eleventyConfig.addFilter('sortCollectionByOrder', (collection) =>
+    collection.sort((a, b) => a.data.order - b.data.order)
+  );
+  eleventyConfig.addFilter('sortCollectionByTitle', (collection) =>
+    collection.sort((a, b) => a.data.title.localeCompare(b.data.title))
+  );
   eleventyConfig.addShortcode('uniqueQueryString', () => `${ Date.now() }`);
-
   return {
-    dir: {
-      input: "src",
-      //includes: "sandbox/_includes",
-      output: "dist"
-    },
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
-  }
-}
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk'
+  };
+};
