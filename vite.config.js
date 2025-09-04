2a4e23317cfa1fea34b8cb9ce740432541c06962
Chris Basham <chris@bash.am>
2025-09-04 11:51:56 -0400

    Clean up and fix scripts

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+import { execSync } from 'child_process';
 import { eleventyPlugin } from 'vite-plugin-eleventy';
 
 export default {
 	root: 'src/sandbox',
 	plugins: [
+		{
+			name: 'style-dictionary-plugin',
+			buildStart() {
+				execSync('npm run build:tokens', { stdio: 'inherit' });
+			}
+		},
 		eleventyPlugin()
 	],
 	build: {
