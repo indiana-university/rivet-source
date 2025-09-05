aa8765741204daa8d10609ca1fb5c5a04ab3c077
Chris Basham <chris@bash.am>
2025-09-04 21:23:25 -0400

    Work on Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,65 +1,43 @@
 import { execSync } from 'child_process';
+import { defineConfig } from 'vite';
 import { eleventyPlugin } from 'vite-plugin-eleventy';
 
-export default {
-	root: 'src/sandbox',
+const styleDictionaryPlugin = {
+	name: 'style-dictionary-plugin',
+	buildStart() {
+		execSync('npm run build:tokens', { stdio: 'inherit' });
+	}
+}
+
+const buildConfig = {
 	plugins: [
-		{
-			name: 'style-dictionary-plugin',
-			buildStart() {
-				execSync('npm run build:tokens', { stdio: 'inherit' });
-			}
-		},
-		eleventyPlugin()
+		styleDictionaryPlugin
 	],
 	build: {
-		outDir: '../../dist',
-		emptyOutDir: true,
-		/*
 		lib: {
-			entry: '../js/index.js',
-			name: 'Rivet',
-			fileName: (format) => `rivet-${format}.js`,
-			formats: ['es', 'esm', 'iife', 'umd']
+			cssFileName: 'rivet',
+			entry: 'src/js/index.js',
+			fileName: (format) => `rivet.${format}.js`,
+			formats: ['es', 'iife', 'umd'],
+			name: 'Rivet'
 		}
-		*/
-	},
+	}
+};
+
+const serveConfig = {
+	root: 'src/sandbox',
+	plugins: [
+		styleDictionaryPlugin,
+		eleventyPlugin()
+	],
 	server: {
 		open: true
 	}
 };
 
-/*
-export default {
-	root: 'src/sandbox',
-	plugins: [
-		{
-			name: 'style-dictionary-plugin',
-			buildStart() {
-				execSync('npm run build:tokens', { stdio: 'inherit' });
-			}
-		},
-		eleventyPlugin()
-	],
-	build: {
-		outDir: '../../dist',
-		emptyOutDir: true,
-		lib: {
-			entry: 'src/js/index.js',
-			name: 'Rivet',
-			fileName: (format) => `rivet.${format}.js`,
-			//formats: ['es', 'umd']
-			formats: ['es']
-		},
-		rollupOptions: {
-			output: {
-				inlineDynamicImports: false,
-				manualChunks: () => null
-			}
-		}
-	},
-	server: {
-		open: true
-	}
+const commandMap = {
+	build: buildConfig,
+	serve: serveConfig
 };
-*/
+
+export default defineConfig(({ command }) => commandMap[command]);
