de1642bdaac3ec0752a812c377a922428273dd48
Chris Basham <chris@bash.am>
2025-09-04 21:51:04 -0400

    Add license banner to out files

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,38 @@
 import { execSync } from 'child_process';
 import { defineConfig } from 'vite';
+import bannerPlugin from 'vite-plugin-banner';
 import { eleventyPlugin } from 'vite-plugin-eleventy';
+import pkg from './package.json';
+
+const libraryName = 'Rivet';
+const fileName = libraryName.toLowerCase();
 
 const styleDictionaryPlugin = {
 	name: 'style-dictionary-plugin',
 	buildStart() {
 		execSync('npm run build:tokens', { stdio: 'inherit' });
 	}
-}
+};
+
+const license = `/*!
+ * ${pkg.name} - @version ${pkg.version}
+ *
+ * Copyright (C) 2018 The Trustees of Indiana University
+ * SPDX-License-Identifier: BSD-3-Clause
+ */`;
 
 const buildConfig = {
 	plugins: [
-		styleDictionaryPlugin
+		styleDictionaryPlugin,
+		bannerPlugin(license)
 	],
 	build: {
 		lib: {
-			cssFileName: 'rivet',
+			cssFileName: fileName,
 			entry: 'src/js/index.js',
-			fileName: (format) => `rivet.${format}.js`,
+			fileName: (format) => `${fileName}.${format}.js`,
 			formats: ['es', 'iife', 'umd'],
-			name: 'Rivet'
+			name: libraryName
 		}
 	}
 };
