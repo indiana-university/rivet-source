09519c038d909e9dd09772374a69cd4bfb85f6ed
Chris Basham <chris@bash.am>
2025-09-30 02:26:31 -0400

    Work on sandbox

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+	devToolbar: {
+		enabled: false
+	},
 	integrations: [mdx()],
 	srcDir: "./src/sandbox-astro"
 });
