ab1ec41c628e4c8a33ef8cd036843ea14760a855
Chris Basham <chris@bash.am>
2025-10-24 10:21:30 -0400

    Remove MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 
 export default defineConfig({
 	devToolbar: {
 		enabled: false,
 	},
-	integrations: [mdx()],
 	srcDir: "./src/sandbox-astro",
 });
