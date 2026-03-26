59546b0cc9800a5ac8617f136c0435d9cd9ea14f
Chris Basham <chris@bash.am>
2026-03-26 13:59:04 -0400

    Clean up menu setup

--- src/prototypes/iub-nav/const.js
+++ src/prototypes/iub-nav/const.js
@@ -1,84 +1,39 @@
-import { getUrl, isCurrentPage } from "./util.js";
-
 export const site = "Indiana University Bloomington";
 
-const menus = [
-	[
-		["IU Bloomington", "home", null],
-		["Academics"],
-		["Admissions", "admissions", true],
-		["Cost & Aid", "cost-aid"],
-		["Campus Life"],
-		["Support & Services"],
-		["Research"],
-		["About IU"],
-		["Alumni & Giving"],
-	],
-	[
-		["Admissions", "admissions", "home"],
-		["Apply", "apply", true],
-		["Admissions Paths", "admissions-paths"],
-		["Admissions Events"],
-		["Visit IU"],
-		["Meet Your Counselors"],
-		["Precollege Programs"],
-		["Planning for IU"],
-		["Class Profile"],
-		["After Admission"],
-		["Admissions Viewbook"],
-		["For Counselors"],
-		["For Families"],
-		["Request Information"],
-	],
-	[
-		["Apply", "apply", "admissions"],
-		["Freshman Applicants", "freshman-applicants"],
-		["Graduate Applicants", "graduate-applicants"],
-		["Returning Applicants"],
-		["Visiting & Non-degree Applicants"],
-		["Transfer Applicants"],
-		["Application Materials"],
-		["Credits & Transfer"],
-		["Manage Your Application"],
-		["How to Apply"],
-	],
-	[
-		["Cost & Aid", "cost-aid", "home"],
-		["Cost of Attendance"],
-		["Financial Aid"],
-		["Scholarships"],
-		["Grants & Fellowships"],
-		["Loans"],
-		["Pay Your Bill"],
-	]
+const applyMenu = [
+	["Freshman Applicants", "freshman-applicants"],
+	["Graduate Applicants", "graduate-applicants"],
+	["Returning Applicants"],
+	["Visiting & Non-degree Applicants"],
+	["Transfer Applicants"],
+	["Application Materials"],
+	["Credits & Transfer"],
+	["Manage Your Application"],
+	["How to Apply"],
 ];
-
-export function getMenus(Astro) {
-	const menuDepth = new Map();
-	return menus.map((menu) => {
-		const [meta, ...menuItems] = menu;
-		const [label, id, parent] = meta;
-		const items = menuItems.map((item) => {
-			const [label, id, child] = item;
-			const current = isCurrentPage(id, Astro);
-			const url = getUrl(id);
-			return { child, current, id, label, url };
-		});
-		const main = !parent;
-		const current = items.some((item) => item.current);
-		const parentDepth = menuDepth.get(parent) || 0;
-		const depth = parentDepth + 1;
-		menuDepth.set(id, depth);
-		const url = getUrl(id);
-		return {
-			current,
-			depth,
-			id,
-			items,
-			label,
-			main,
-			parent,
-			url,
-		};
-	});
-}
+const admissionsMenu = [
+	["Apply", "apply", applyMenu],
+	["Admissions Paths", "admissions-paths"],
+	["Admissions Events"],
+	["Visit IU"],
+	["Meet Your Counselors"],
+	["Precollege Programs"],
+	["Planning for IU"],
+	["Class Profile"],
+	["After Admission"],
+	["Admissions Viewbook"],
+	["For Counselors"],
+	["For Families"],
+	["Request Information"],
+];
+const homeMenu = [
+	["Academics"],
+	["Admissions", "admissions", admissionsMenu],
+	["Cost & Aid", "cost-aid"],
+	["Campus Life"],
+	["Support & Services"],
+	["Research"],
+	["About IU"],
+	["Alumni & Giving"],
+]
+export const menus = ["IU Bloomington", "home", homeMenu];
--- src/prototypes/iub-nav/util.js
+++ src/prototypes/iub-nav/util.js
@@ -1,5 +1,48 @@
 import { removeEnd } from "@src/utilities/removeEnd.js";
 
+export function getMenus(menus, Astro) {
+	const all = [];
+
+	function getMenu(data, depth = 0, parent = null) {
+		const [label, id, menuItems = []] = data;
+		const main = depth === 0;
+		const items = menuItems.map((item) => getMenu(item, depth + 1, id));
+		const hasCurrent = items.some((item) => item.current);
+		const hasChildren = !!items.length;
+		const current = isCurrentPage(id, Astro);
+		const url = id ? getUrl(id) : "#";
+		const menu = {
+			current,
+			depth,
+			hasChildren,
+			hasCurrent,
+			id,
+			items,
+			label,
+			main,
+			parent,
+			url,
+		};
+		if (hasChildren) {
+			all.push(menu);
+		}
+		return menu;
+	}
+
+	getMenu(menus);
+
+	const main = all.find((menu) => menu.main);
+	const current = all.find((menu) => menu.hasCurrent) || main;
+	const home = isCurrentPage("home", Astro);
+
+	return {
+		all,
+		current,
+		home,
+		main,
+	};
+}
+
 export function getUrl(path) {
 	return [
 		import.meta.env.BASE_URL,
