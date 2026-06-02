dff51130719c2002946a78ad6789c8804a19d96d
Zachary Todd <zttodd@iu.edu>
2026-06-02 13:47:06 -0400

    Set up script for running 'add-license' manually; more specific license text check

--- scripts/add-license.js
+++ scripts/add-license.js
@@ -11,14 +11,14 @@ const rawLines = licenseText.split("\n");
 const commentLines = rawLines.map(line => ` * ${line}`);
 const header = `/*\n${commentLines.join("\n")}\n */`;
 
-// Store git staged files from lint-staged
+// Get array of files via standalone command or lint-staged
 const files = process.argv.slice(2);
 
 for (const file of files) {
 	const contents = readFileSync(file, "utf8");
 	const firstFewLines = contents.split("\n").slice(0, 6).join("\n");
 
-	if (!firstFewLines.includes("SPDX-License-Identifier")) {
+	if (!firstFewLines.includes(header)) {
 		writeFileSync(file, `${header}\n\n${contents}`);
 		console.log(`${ANSI.green}+${ANSI.reset} Added license: ${file}`);
 	}
