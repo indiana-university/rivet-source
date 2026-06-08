a0db8bd6d337a53ef3400cde149289503c67b283
Zachary Todd <zttodd@iu.edu>
2026-06-08 14:08:59 -0400

    Fixed formatting issues using lint-staged

--- scripts/utils/ansi-colors.js
+++ scripts/utils/ansi-colors.js
@@ -1,4 +1,4 @@
 export const ANSI = {
-    green: "\x1b[32m",
-    reset: "\x1b[0m"
+	green: "\x1b[32m",
+	reset: "\x1b[0m",
 };
--- scripts/utils/license-header.js
+++ scripts/utils/license-header.js
@@ -2,7 +2,10 @@ const raw = `Copyright (C) 2018 The Trustees of Indiana University
 SPDX-License-Identifier: BSD-3-Clause`;
 
 // Output license as block comment body only
-const body = raw.split("\n").map(line => ` * ${line}`).join("\n");
+const body = raw
+	.split("\n")
+	.map((line) => ` * ${line}`)
+	.join("\n");
 
 // Output license as full multi-line/block comment
 const header = `/*\n${body}\n */\n\n`;
