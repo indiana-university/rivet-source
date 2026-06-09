bc8d196c514b03b3cdba886acadd04661fbbc8cf
Zachary Todd <zttodd@iu.edu>
2026-06-09 14:44:01 -0400

    Updated remaining CSS, JS, and Sass files returned from grep searches

--- src/utilities/baseUrl.js
+++ src/utilities/baseUrl.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2018 The Trustees of Indiana University
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 import { removeStart } from "./removeStart.js";
 
 export function baseUrl(path) {
--- src/utilities/removeStart.js
+++ src/utilities/removeStart.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2018 The Trustees of Indiana University
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 export function removeStart(str, start) {
 	const startsWith = new RegExp(`^${start}`);
 	return str.replace(startsWith, "");
