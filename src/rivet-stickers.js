6f2965b1cf9a2d42d37bc09018117610d36cc324
Zachary Todd <zttodd@iu.edu>
2026-06-09 12:00:13 -0400

    Manually updated license header blocks

--- src/rivet-stickers.js
+++ src/rivet-stickers.js
@@ -1,6 +1,6 @@
-/******************************************************************************
+/*
  * Copyright (C) 2018 The Trustees of Indiana University
  * SPDX-License-Identifier: BSD-3-Clause
- *****************************************************************************/
+ */
 
 import "./rivet-stickers.css";
--- src/utilities/remToPixel.js
+++ src/utilities/remToPixel.js
@@ -1,7 +1,7 @@
-/******************************************************************************
- * Copyright (C) 2024 The Trustees of Indiana University
+/*
+ * Copyright (C) 2018 The Trustees of Indiana University
  * SPDX-License-Identifier: BSD-3-Clause
- *****************************************************************************/
+ */
 
 export function remToPixel(rem) {
 	return parseInt(rem) * 16;
