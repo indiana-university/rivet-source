e8e6ff1624b0acc7c8b17fff87b8856ba89af819
Chris Basham <csbasham@iu.edu>
2026-06-12 15:55:37 -0400

    Fix iub-nav prototype

--- examples/iub-nav/src/const.js
+++ examples/iub-nav/src/const.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2018 The Trustees of Indiana University
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 export const site = "Indiana University Bloomington";
 
 const applyMenu = [
@@ -36,4 +41,4 @@ const homeMenu = [
 	["About IU"],
 	["Alumni & Giving"],
 ];
-export const menus = ["IU Bloomington", "home", homeMenu];
+export const menus = [site, "", homeMenu];
