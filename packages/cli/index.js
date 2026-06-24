7f528d600e184f68b6905bcb81bdf119c0f13ec8
Zachary Todd <zttodd@iu.edu>
2026-06-24 15:56:44 -0400

    Set up entry index.js for CLI tool

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+
+import { Command } from "commander";
+import pkg from "./package.json" with { type: "json" };
+
+const program = new Command();
+
+program
+	.name("rivet")
+	.description(`${pkg.description}`)
+	.version(`${pkg.version}`);
+
+program.parse();
